9c2937e14bd314b079144ed4aa2ee390f5704bdd
Leatpt <lea.kibur@tptlive.ee>
2024-05-05 20:26:12 +0300

    adding sass

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+import "./styles/index.scss";
 console.log("where now?");
+
+const obj = {
+    a: "apple crisp",
+    b: "buttercream",
+    c: {
+        cd: "cheese danish",
+    },
+};
+
+console.log(obj?.c?.cd, obj?.f?.fa);
 
 if (module.hot) {
     module.hot.accept();
